b6a0d8012fce14e116ab2767d43a5647a5386900
guikoller <g_koller@hotmail.com>
2023-02-06 19:02:06 -0300

    button component

--- 1/react-fundamentals/src/App.js
+++ 1/react-fundamentals/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Button from "./Components/Button/Button"
 
 function App() {
   return (
+    <div>
       <Navbar/>
+      <div style={
+        {
+          margin: '100px 0 0 100px'
+        }
+      }>
+        <Button/>
+      </div>
+      
+    </div>      
   );
 }
 
---
+++ 1/react-fundamentals/src/Components/Button/Button.js
@@ -0,0 +1,9 @@
+import "./style.css"
+
+function Navbar() {
+    return (
+        <button type="button" class="PixellButton">Button</button>
+    );
+}
+
+export default Navbar
